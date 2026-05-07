ebebdac8ce71a2eb8257737555f0ba348460b343
Debby Kenza <nicouebeglahkenza@gmail.com>
2026-05-07 10:18:18 +0200

    ajout de vercel.json

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+self.addEventListener('fetch', function(event) {
+    if(event.request.url.includes('articles-api.com/articles')) {
+        event.respondWith(
+            caches.match(event.request).then(function(response) {
+                return response || fetch(event.request).then(function(networkResponse) {
+                    return caches.open('articles-cache').then(function(cache) {
+                        cache.put(event.request, networkResponse.clone())
+                        return response;
+                    })
+                })
+            })
+        )
+    }
+})
